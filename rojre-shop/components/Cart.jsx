df683282163b8e97cce5c962ef4d6940da430559
Robert Regalado <rjregalado1979@gmail.com>
2022-08-31 17:34:42 +0800

    Add StateContext.js file

--- rojre-shop/components/Cart.jsx
+++ rojre-shop/components/Cart.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import Link from 'next/link';
+import { AiOutlineMinus, AiOutlinePlus, AiOutlineLeft, AiOutlineShopping } from 'react-icons/ai';
+import { TiDeleteOutline } from 'react-icons/ti';
+import toast from 'react-hot-toast';
+
+import { useStateContext } from '../context/StateContext';
+import { urlFor } from '../lib/client';
 
 const Cart = () => {
+  const cartRef = useRef();
+  const { totalPrice, totalQuantities, cartItems, setShowCart } = useStateContext();
+
   return (
-    <div>
-      
+    <div className='cart-wrapper' ref={cartRef}>
+      <div className='cart-container'>
+        <button
+          type='button'
+          className="cart-heading"
+          onClick={() => setShowCart(false)}>
+          <AiOutlineLeft />
+          <span className='heading'>Your Cart</span>
+          <span className='cart-num-items'>({totalQuantities}) items</span>
+        </button>
+
+        {cartItems.length < 1 && (
+          <div className='empty-cart'>
+            <AiOutlineShopping size={150} />
+            <h3>Your shopping bag is empty</h3>
+            <Link href={"/"}>
+              <button type='button'
+                onClick={() => setShowCart(false)}
+                className="btn"
+              >
+                Continue Shopping
+              </button>
+            </Link>
+          </div>
+        )}
+        <div className='product-container'>
+          {cartItems.length >= 1 && cartItems.map((item) => (
+            <div className='product' key={item._id}>
+              <img src={urlFor(item?.image[0])}
+                className="cart-product-image" />
+              <h5>{item.name}</h5>
+              <h4>{item.price}</h4>
+            </div>
+            
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
--- rojre-shop/pages/product/[slug].js
+++ rojre-shop/pages/product/[slug].js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
 
 import { client, urlFor } from '../../lib/client';
+import { Product } from '../../components';
+import { useStateContext } from '../../context/StateContext';
 
 const ProductDetails = ({ product, products }) => {
     const { image, name, details, price } = product;
+    const [index, setIndex] = useState(0);
+    const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
 
     return (
         <div>
@@ -12,17 +16,21 @@ const ProductDetails = ({ product, products }) => {
                 className='product-detail-container'>
                 <div>
                     <div className='image-container'>
-                        <img src={urlFor(image && image[0])} />
+                        <img src={urlFor(image && image[index])}
+                            className="product-detail-image" />
                     </div>
-                    {/*<div className='small-images-container'>
+                    <div className='small-images-container'>
                         {image?.map((item, i) => (
                             <img
                                 src={urlFor(item)}
-                                className=""
-                                onMouseEnter=""
+                                className={i == index ?
+                                    'small-image selected-image' :
+                                    'small-image'}
+                                onMouseEnter={() => setIndex(i)
+                                }
                             />
                         ))}
-                        </div>*/}
+                    </div>
                 </div>
                 <div className='product-detail-desc'>
                     <h1>{name}</h1>
@@ -45,26 +53,39 @@ const ProductDetails = ({ product, products }) => {
                         <h3>Quantity</h3>
                         <p className='quantity-desc'>
                             <span className='minus'
-                                onClick=""><AiOutlineMinus />
+                                onClick={decQty}><AiOutlineMinus />
                             </span>
                             <span className='num'
-                                onClick="">0</span>
+                                onClick="">{qty}</span>
                             <span className='plus'
-                                onClick=""><AiOutlinePlus />
+                                onClick={incQty}><AiOutlinePlus />
                             </span>
                         </p>
                     </div>
                     <div className='buttons'>
                         <button type='button'
                             className='add-to-cart'
-                            onClick="">Add to Cart</button>
+                            onClick={() => onAdd(product, qty)}>Add to Cart</button>
 
                         <button type='button'
                             className='buy-now'
                             onClick="">Buy Now</button>
                     </div>
                 </div>
+            </div>
 
+            <div
+                className='maylike-products-wrapper'>
+                <h2>You may also like</h2>
+                <div className='marquee'>
+                    <div
+                        className='maylike-products-container track'>
+                        {products.map((item) => (
+                            <Product key={item._id}
+                                product={item} />
+                        ))}
+                    </div>
+                </div>
             </div>
         </div>
     )
